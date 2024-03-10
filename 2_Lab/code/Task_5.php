8eef52588ff94bb52c36bb8a07548ada5d6bf048
Arseniy Ott <arseniy.ott@mail.ru>
2024-03-10 15:42:39 +0200

    Create Task_5.php [Lab_2]

---
+++ 2_Lab/code/Task_5.php
@@ -0,0 +1,13 @@
+<?php
+echo ("\nTask - 5 ____________\n");
+
+$my_num = PHP_INT_MAX;
+$answer = $my_num;
+var_dump($answer); // Первоначально тип у переменной int
+$answer += 2;      // При переполнении она превращается в float
+var_dump($answer);
+$answer *= 2;
+$answer -= 2;
+$answer /= 2;
+$answer -= $my_num; //Значение должно быть равно 1, однако из-за точности представления чисел с плавающей точкой значение будет равно нулю)0)
+echo $answer."\n";
