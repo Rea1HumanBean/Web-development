97d27fe059034d224a8c788d930546f14ea7837b
Arseniy Ott <arseniy.ott@mail.ru>
2024-03-10 15:42:16 +0200

    Create Task_2.php [Lab_2]

---
+++ 2_Lab/code/Task_2.php
@@ -0,0 +1,16 @@
+<?php
+echo ("\nTask - 2 ____________\n");
+
+$week_day = 7;
+echo $week_day;
+echo ("\n");
+
+$half_week_day = 3.5;
+echo ($half_week_day);
+
+echo ("\n".(6 + 6));
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+
+echo ("\n"."Difference: ".($last_month - $this_month));
