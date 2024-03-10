59cc2c8c4ecfaec196f8b24d9d9c213f0fc5e41a
Arseniy Ott <arseniy.ott@mail.ru>
2024-03-10 15:41:25 +0200

    Update index.php [lab_2]

--- 2_Lab/code/index.php
+++ 2_Lab/code/index.php
@@ -1,49 +1,9 @@
 <?php
-//Task - 1
 
-/* Imagine a lot of code here */
-$very_bad_unclear_name = "15 chicken wings";
+require 'Task_1.php';
+require 'Task_2.php';
+require 'Task_3.php';
+require 'Task_4.php';
+require 'Task_5.php';
+require 'Task_6.php';
 
-// Write your code here:
-
-$order =& $very_bad_unclear_name;
-$order .= ' & Ice Cube';
-
-// Don't change the line below
-echo "\nYour order is: $very_bad_unclear_name."; // В данном случае будет выведена строка "15 chicken wings & Ice Cube" ,т.к. переменные указывают на одну и ту же область в памяти
-
-//Task - 2
-$week_day = 7;
-echo "\n" . $week_day;
-echo ("\n");
-$half_week_day = 3.5;
-echo ($half_week_day);
-echo ("\n".(6 + 6));
-
-$last_month = 1187.23;
-$this_month = 1089.98;
-
-echo ("\n"."Difference: ".($last_month - $this_month));
-
-//Task - 3
-$num_languages = 4;
-$months = 11;
-$days = $months * 16;
-
-$days_per_language = $days / $num_languages;
-echo "\n"."The average number of days of the study: ".($days_per_language);
-
-//Task - 4
-echo "\n".(8**2)."\n";
-
-//Task - 5
-$my_num = PHP_INT_MAX;
-$answer = $my_num;
-var_dump($answer); // Первоначально тип у переменной int
-$answer += 2;      // При переполнении она превращается в float
-$answer *= 2;
-$answer -= 2;
-$answer /= 2;
-$answer -= $my_num; //Значение должно быть равно 1, однако из-за точности представления чисел с плавающей точкой значение будет равно нулю)0)
-echo $answer."\n";
-var_dump($answer);
