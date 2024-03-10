64f9990ae7e935399ab0809f2ef317f6c6d9b490
Arseniy Ott <arseniy.ott@mail.ru>
2024-03-10 15:42:23 +0200

    Create Task_3.php [Lab_2]

---
+++ 2_Lab/code/Task_3.php
@@ -0,0 +1,9 @@
+<?php
+echo ("\nTask - 3 ____________\n");
+
+$num_languages = 4;
+$months = 11;
+$days = $months * 16;
+
+$days_per_language = $days / $num_languages;
+echo "\n"."The average number of days of the study: ".($days_per_language);
