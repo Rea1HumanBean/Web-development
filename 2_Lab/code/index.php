059dc4bb389d7ed6e343ba68c5d089e36c2c3901
Arseniy Ott <arseniy.ott@mail.ru>
2024-03-10 20:10:39 +0200

    Update index.php [Lab_2]

--- 2_Lab/code/index.php
+++ 2_Lab/code/index.php
@@ -6,4 +6,9 @@ require 'Task_3.php';
 require 'Task_4.php';
 require 'Task_5.php';
 require 'Task_6.php';
+require 'Task_7.php';
+require 'Task_8.php';
+require 'Task_9.php';
+require 'Task_10.php';
+require 'Task_11.php';
 
