6bf80cba05546032eae0b40ead4d70a9c4f00f98
Arseniy Ott <arseniy.ott@mail.ru>
2024-03-31 02:42:26 +0200

    Create InfoPeson [Lab_3]

---
+++ 3_Lab/code/Task_2.3/InfoPerson.php
@@ -0,0 +1,14 @@
+<?php
+function redirectToHome(): void {
+	header("Location: /");
+	exit();
+}
+
+if (false === isset($_POST['surname'], $_POST['name'], $_POST['age'], $_POST['profession'], $_POST['salary'])) {
+	redirectToHome();
+}
+
+session_start();
+$_SESSION["user_data"] = [$_POST['surname'], $_POST['name'], $_POST['age'], $_POST['profession'], $_POST['salary']];
+
+redirectToHome();
