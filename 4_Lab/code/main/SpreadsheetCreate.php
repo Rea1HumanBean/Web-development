0ba2286fe1dae51939d7429c82e813ba2b4abf22
Arseniy Ott <arseniy.ott@mail.ru>
2024-06-13 21:54:23 +0200

    Update SpreadsheetCreate.php

--- 4_Lab/code/main/SpreadsheetCreate.php
+++ 4_Lab/code/main/SpreadsheetCreate.php
@@ -6,8 +6,6 @@ include 'posFirstFreeCell.php';
 require __DIR__.'/../../vendor/autoload.php';
 
 use Google\Client as Google_Client;
-use Google\Spreadsheet\ServiceRequestFactory;
-use Google\Spreadsheet\DefaultServiceRequest;
 
 function redirectToHome(): void {
 	header('Location: index.php');
@@ -16,17 +14,17 @@ function redirectToHome(): void {
 
 try{
 	$client = new Google_Client;
-	$client->setApplicationName("LAB_4"); //Любое название
+	$client->setApplicationName("LAB_4");
 	$client->setScopes([Google_Service_Sheets::SPREADSHEETS]);
 	$client->addScope('https://www.googleapis.com/auth/spreadsheets');
-	$client->setAuthConfig(__DIR__.'/credentials.json'); //там где учётка хранится и с каким именем
+	$client->setAuthConfig(__DIR__.'/credentials.json');
 	$client->setAccessType("offline");
 	$client->useApplicationDefaultCredentials();
 
 	$service = new Google_Service_Sheets($client);
-	$spreadsheetId = '1J-pwaAyc7dVmDIrF26H_7s-aZLLoOAvrXuykSU8Qwu4'; // ID-шку сюда вставляем
+	$spreadsheetId = '';
 
-	$response = $service->spreadsheets_values->get($spreadsheetId, 'Лист1!A1:D'); // название листа у вас такое же как и таблицу
+	$response = $service->spreadsheets_values->get($spreadsheetId, 'Лист1!A1:D');
 	$values = $response->getValues();
 
 	$row = findFirstEmptyRow($values);
